criupatchwork/criu

commit fef01d79f7ee37659185a307052604eb88958677

compel: cli -- Add more flags into cflags report
Signed-off-by: Cyrill Gorcunov <gorcunov@openvz.org>
Signed-off-by: Pavel Emelyanov <xemul@virtuozzo.com>

compel/src/main.c
@@ -15,8 +15,14 @@
 #include "version.h"
 #include "piegen.h"
 
-static const char compel_cflags_pie[] = "-fpie -Wa,--noexecstack -fno-stack-protector";
-static const char compel_cflags_nopic[] = "-fno-pic -Wa,--noexecstack -fno-stack-protector";
+static const char compel_cflags_pie[] =
+	"-fpie -Wstrict-prototypes -Wa,--noexecstack "
+	"-fno-stack-protector -fno-jump-tables -nostdlib "
+	"-fomit-frame-pointer";
+static const char compel_cflags_nopic[] =
+	"-fno-pic -Wstrict-prototypes -Wa,--noexecstack "
+	"-fno-stack-protector -fno-jump-tables -nostdlib "
+	"-fomit-frame-pointer";
 static const char compel_ldflags[] = "-r";
 
 piegen_opt_t opts = {

commit c13775432bf90410e30ab07a8ff36791c4c3cf7d

compel: fix build break on ppc64le
This patch fix a build break on ppc64le introduced by changes in the
compel's build option.

I factorized the common CFLAGS to make easier identifying particular
flags required by some architectures.

Fixes: fef01d79f7ee ("compel: cli -- Add more flags into cflags report")
Cc: Cyrill Gorcunov <gorcunov@openvz.org>
Signed-off-by: Laurent Dufour <ldufour@linux.vnet.ibm.com>

compel/src/main.c
@@ -15,15 +15,15 @@
 #include "version.h"
 #include "piegen.h"
 
-static const char compel_cflags_pie[] =
-	"-fpie -Wstrict-prototypes -Wa,--noexecstack "
-	"-fno-stack-protector -fno-jump-tables -nostdlib "
-	"-fomit-frame-pointer";
-static const char compel_cflags_nopic[] =
-	"-fno-pic -Wstrict-prototypes -Wa,--noexecstack "
-	"-fno-stack-protector -fno-jump-tables -nostdlib "
-	"-fomit-frame-pointer";
-static const char compel_ldflags[] = "-r";
+#define CFLAGS_DEFAULT_SET					\
+	"-Wstrict-prototypes -Wa,--noexecstack "		\
+	"-fno-stack-protector -nostdlib -fomit-frame-pointer "
+
+#define COMPEL_CFLAGS_PIE	CFLAGS_DEFAULT_SET "-fpie -fno-jump-tables"
+#define COMPEL_CFLAGS_PIE_JPT	CFLAGS_DEFAULT_SET "-fpie"
+#define COMPEL_CFLAGS_NOPIC	CFLAGS_DEFAULT_SET "-fno-pic -fno-jump-tables"
+
+#define COMPEL_LDFLAGS_DEFAULT "-r"
 
 piegen_opt_t opts = {
 	.input_filename		= NULL,
@@ -99,19 +99,19 @@ int main(int argc, char *argv[])
 	static const compel_cflags_t compel_cflags[] = {
 		{
 			.arch	= "x86",
-			.cflags	= compel_cflags_pie,
+			.cflags	= COMPEL_CFLAGS_PIE,
 		}, {
 			.arch	= "ia32",
-			.cflags	= compel_cflags_nopic,
+			.cflags	= COMPEL_CFLAGS_NOPIC,
 		}, {
 			.arch	= "aarch64",
-			.cflags	= compel_cflags_pie,
+			.cflags	= COMPEL_CFLAGS_PIE,
 		}, {
 			.arch	= "arm",
-			.cflags	= compel_cflags_pie,
+			.cflags	= COMPEL_CFLAGS_PIE,
 		}, {
 			.arch	= "ppc64",
-			.cflags	= compel_cflags_pie,
+			.cflags	= COMPEL_CFLAGS_PIE_JPT,
 		},
 	};
 
@@ -190,7 +190,7 @@ int main(int argc, char *argv[])
 	}
 
 	if (!strcmp(action, "ldflags")) {
-		printf("%s", compel_ldflags);
+		printf("%s", COMPEL_LDFLAGS_DEFAULT);
 		return 0;
 	}
 