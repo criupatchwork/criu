criupatchwork/criu

commit 6fde3b8c27db32a790254034e73b961683aed0d0

x86: restore TLS
Put dumped TLS descriptors back to GDT.
Do it only if it was present.

Cc: Cyrill Gorcunov <gorcunov@openvz.org>
Signed-off-by: Dmitry Safonov <dsafonov@virtuozzo.com>
Signed-off-by: Pavel Emelyanov <xemul@virtuozzo.com>

criu/arch/x86/include/asm/restore.h
@@ -28,7 +28,28 @@
 	;
 #endif /* CONFIG_X86_64 */
 
-#define core_get_tls(pcore, ptls)
+static inline void core_get_tls(CoreEntry *pcore, tls_t *ptls)
+{
+	ThreadInfoX86 *ti = pcore->thread_info;
+	int i;
+
+	for (i = 0; i < GDT_ENTRY_TLS_NUM; i++) {
+		user_desc_t *to = &ptls->desc[i];
+		UserDescT *from = ti->tls[i];
+
+#define COPY_TLS(field) to->field = from->field
+		COPY_TLS(entry_number);
+		COPY_TLS(base_addr);
+		COPY_TLS(limit);
+		COPY_TLS(seg_32bit);
+		to->contents = ((u32)from->contents_h << 1) | from->contents_l;
+		COPY_TLS(read_exec_only);
+		COPY_TLS(limit_in_pages);
+		COPY_TLS(seg_not_present);
+		COPY_TLS(useable);
+#undef COPY_TLS
+	}
+}
 
 
 int restore_fpu(struct rt_sigframe *sigframe, CoreEntry *core);

criu/arch/x86/include/asm/restorer.h
@@ -334,10 +334,7 @@ int restore_nonsigframe_gpregs(UserX86RegsEntry *r);
 int sigreturn_prep_fpu_frame(struct rt_sigframe *sigframe,
 		struct rt_sigframe *rsigframe);
 
-static inline void restore_tls(tls_t *ptls)
-{
-	(void)ptls;
-}
+void restore_tls(tls_t *ptls);
 
 int ptrace_set_breakpoint(pid_t pid, void *addr);
 int ptrace_flush_breakpoints(pid_t pid);

criu/arch/x86/restorer.c
@@ -4,6 +4,7 @@
 #include "restorer.h"
 #include "asm/restorer.h"
 #include "asm/fpu.h"
+#include "asm/string.h"
 
 #include "syscall.h"
 #include "log.h"
@@ -30,4 +31,65 @@ int restore_nonsigframe_gpregs(UserX86RegsEntry *r)
 	}
 #endif
 	return 0;
+}
+
+extern unsigned long call32_from_64(void *stack, void *func);
+
+asm (	"	.pushsection .text				\n"
+	"	.global restore_set_thread_area			\n"
+	"	.code32						\n"
+	"restore_set_thread_area:				\n"
+	"	movl $"__stringify(__NR32_set_thread_area)",%eax\n"
+	"	int $0x80					\n"
+	"	ret						\n"
+	"	.popsection					\n"
+	"	.code64");
+extern char restore_set_thread_area;
+
+static void *stack32;
+
+static int prepare_stack32(void)
+{
+
+	if (stack32)
+		return 0;
+
+	stack32 = (void*)sys_mmap(NULL, PAGE_SIZE, PROT_READ | PROT_WRITE,
+				MAP_32BIT | MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
+	if (stack32 == MAP_FAILED) {
+		stack32 = NULL;
+		pr_err("Failed to allocate stack for 32-bit TLS restore\n");
+		return -1;
+	}
+
+	return 0;
+}
+
+void restore_tls(tls_t *ptls)
+{
+	int i;
+
+	for (i = 0; i < GDT_ENTRY_TLS_NUM; i++) {
+		user_desc_t *desc = &ptls->desc[i];
+		int ret;
+
+		if (desc->seg_not_present)
+			continue;
+
+		if (prepare_stack32() < 0)
+			return;
+
+		builtin_memcpy(stack32, desc, sizeof(user_desc_t));
+
+		/* user_desc parameter for set_thread_area syscall */
+		asm volatile ("\t movl %%ebx,%%ebx\n" : :"b"(stack32));
+		call32_from_64(stack32 + PAGE_SIZE, &restore_set_thread_area);
+		asm volatile ("\t movl %%eax,%0\n" : "=r"(ret));
+		if (ret)
+			pr_err("Failed to restore TLS descriptor %d in GDT ret %d\n",
+					desc->entry_number, ret);
+	}
+
+	if (stack32)
+		sys_munmap(stack32, PAGE_SIZE);
 }

criu/pie/Makefile
@@ -14,6 +14,7 @@ restorer-obj-y		+= ./$(ARCH_DIR)/restorer.o
 
 ifeq ($(ARCH),x86)
         restorer-obj-e		+= ./$(ARCH_DIR)/syscalls-64.built-in.o
+        restorer-obj-y		+= ./$(ARCH_DIR)/call32.o
 
         native-obj-y		+= ./$(ARCH_DIR)/parasite-head-64.o
         native-obj-e		+= ./$(ARCH_DIR)/syscalls-64.built-in.o

commit 784235cb7de3c5c800ef10f3f93a8e50166f8217

x86, tls: read no more than saved TLS entries
While writing this, I somehow managed to miss the check of
how many entries were saved in core image.
So it may dereference here bs.

Fixes: #228
Fixes: commit 6fde3b8c27db ("x86: restore TLS")

Cc: Andrei Vagin <avagin@virtuozzo.com>
Cc: Cyrill Gorcunov <gorcunov@openvz.org>
Reported-by: Andrei Vagin <avagin@virtuozzo.com>
Signed-off-by: Dmitry Safonov <dsafonov@virtuozzo.com>

criu/arch/x86/include/asm/restore.h
@@ -31,12 +31,22 @@
 static inline void core_get_tls(CoreEntry *pcore, tls_t *ptls)
 {
 	ThreadInfoX86 *ti = pcore->thread_info;
-	int i;
+	size_t i;
 
 	for (i = 0; i < GDT_ENTRY_TLS_NUM; i++) {
 		user_desc_t *to = &ptls->desc[i];
-		UserDescT *from = ti->tls[i];
+		UserDescT *from;
 
+		/*
+		 * If proto image has lesser TLS entries,
+		 * mark them as not present (and thus skip restore).
+		 */
+		if (i >= ti->n_tls) {
+			to->seg_not_present = 1;
+			continue;
+		}
+
+		from = ti->tls[i];
 #define COPY_TLS(field) to->field = from->field
 		COPY_TLS(entry_number);
 		COPY_TLS(base_addr);