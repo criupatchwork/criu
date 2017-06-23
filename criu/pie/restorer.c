criupatchwork/criu

commit 8544895a528b78878747d13ab24bb6a23c883ebf

ia32/restorer: move 32-bit pie unmap to x86
We need compat realization for restorer unmap as after rt_sigreturn()
the task is stopped it 32-bit code and ptrace API doesn't allow
setting x86_64 full registers set to ia32 task.

Generic restorer has now x86-specific __export_unmap_compat()
function, which isn't right.

Clean restorer from x86-related realization.

Signed-off-by: Dmitry Safonov <dsafonov@virtuozzo.com>
Signed-off-by: Andrei Vagin <avagin@virtuozzo.com>

criu/arch/x86/restorer_unmap.S
@@ -1,0 +1,12 @@
+#include "common/asm/linkage.h"
+#include "compel/plugins/std/syscall-codes.h"
+
+	.text
+ENTRY(__export_unmap_compat)
+	.code32
+	mov	bootstrap_start, %ebx
+	mov	bootstrap_len, %ecx
+	movl	$__NR32_munmap, %eax
+	int	$0x80
+	int	$0x03 /* Guard */
+	.code64

criu/pie/restorer.c
@@ -929,34 +929,13 @@ static unsigned long vdso_rt_size;
 #define vdso_rt_size	(0)
 #endif
 
-static void *bootstrap_start;
-static unsigned int bootstrap_len;
+void *bootstrap_start = NULL;
+unsigned int bootstrap_len = 0;
 
 void __export_unmap(void)
 {
 	sys_munmap(bootstrap_start, bootstrap_len - vdso_rt_size);
 }
-
-#ifdef CONFIG_X86_64
-asm (
-	"	.pushsection .text\n"
-	"	.global	__export_unmap_compat\n"
-	"__export_unmap_compat:\n"
-	"	.code32\n"
-	"	mov bootstrap_start, %ebx\n"
-	"	mov bootstrap_len, %ecx\n"
-	"	sub vdso_rt_size, %ecx\n"
-	"	movl $"__stringify(__NR32_munmap)", %eax\n"
-	"	int	$0x80\n"
-	"	.code64\n"
-	"	.popsection\n"
-);
-extern char __export_unmap_compat;
-#else
-void __export_unmap_compat(void)
-{
-}
-#endif
 
 /*
  * This function unmaps all VMAs, which don't belong to

criu/pie/Makefile
@@ -25,6 +25,7 @@ restorer-obj-y	+= ./$(ARCH_DIR)/restorer.o
 ifeq ($(ARCH),x86)
         ifeq ($(CONFIG_COMPAT),y)
                 restorer-obj-y	+= ./$(ARCH_DIR)/call32.o
+                restorer-obj-y	+= ./$(ARCH_DIR)/restorer_unmap.o
                 restorer-obj-y	+= ./$(ARCH_DIR)/sigaction_compat_pie.o
         endif
 endif

commit 2a7e1260e5ced38c03f21c7a1b8b8c7d53dae888

vdso/ia32: Don't unmap rt-vdso
Found with fault-injected jump trampolines in vdso,
that on ia32 tests rt-vdso got unmapped.
I've fixed it previously, but have forgot it during
moving 32-bit pie's unmap into arch/x86/

Fixes: commit 8544895a528b ("ia32/restorer: move 32-bit pie unmap to x86")
Signed-off-by: Dmitry Safonov <dsafonov@virtuozzo.com>

criu/arch/x86/restorer_unmap.S
@@ -6,6 +6,7 @@ ENTRY(__export_unmap_compat)
 	.code32
 	mov	bootstrap_start, %ebx
 	mov	bootstrap_len, %ecx
+	sub	vdso_rt_size, %ecx
 	movl	$__NR32_munmap, %eax
 	int	$0x80
 	int	$0x03 /* Guard */

criu/pie/restorer.c
@@ -1040,7 +1040,7 @@ static void restore_posix_timers(struct task_restore_args *args)
  * trap us on the exit from sys_munmap.
  */
 #ifdef CONFIG_VDSO
-static unsigned long vdso_rt_size;
+unsigned long vdso_rt_size = 0;
 #else
 #define vdso_rt_size	(0)
 #endif