af9d7a4c75273bc76ef348bfe21fa4d5e9bcb072
Cyrill Gorcunov <gorcunov@gmail.com>
2018-04-03 20:46:50 +0300

    files: Move make_gen_id into separate kcmp_fd_make_gen_id helper
    We will need it for epoll targets.
    
    Signed-off-by: Cyrill Gorcunov <gorcunov@gmail.com>

--- criu/include/file-ids.h
+++ criu/include/file-ids.h
@@ -1,6 +1,8 @@
 #ifndef __CR_FILE_IDS_H__
 #define __CR_FILE_IDS_H__
 
+#include <stdint.h>
+
 #include "common/compiler.h"
 #include "rbtree.h"
 
@@ -15,5 +17,17 @@ struct stat;
 struct fd_parms;
 extern int fd_id_generate(pid_t pid, FdinfoEntry *fe, struct fd_parms *p);
 extern int fd_id_generate_special(struct fd_parms *p, u32 *id);
+
+/*
+ * The gen_id thing is used to optimize the comparison of shared files.
+ * If two files have different gen_ids, then they are different for sure.
+ * If it matches, we don't know it and have to call sys_kcmp().
+ *
+ * The kcmp-ids.c engine does this trick, see comments in it for more info.
+ */
+static inline uint32_t kcmp_fd_make_gen_id(uint32_t st_dev, uint32_t st_ino, uint32_t f_pos)
+{
+	return st_dev ^ st_ino ^ f_pos;
+}
 
 #endif /* __CR_FILE_IDS_H__ */
