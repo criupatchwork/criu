917916eb58c546dc7a6651aef6e316bf67a515a3
Cyrill Gorcunov <gorcunov@gmail.com>
2018-10-09 16:49:18 +0300

    net: libnetlink -- Call error handler instead of unconditional error message
    To be able to zap error messages when needed. Without error
    handler we will continue printing error. This will placate
    diag modules probing since they are not mandatory features.
    
    Signed-off-by: Cyrill Gorcunov <gorcunov@gmail.com>

--- criu/libnetlink.c
+++ criu/libnetlink.c
@@ -21,10 +21,8 @@ static int nlmsg_receive(char *buf, int len,
 			continue;
 		if (hdr->nlmsg_type == NLMSG_DONE) {
 			int *len = (int *)NLMSG_DATA(hdr);
-
 			if (*len < 0)
-				return *len;
-
+				return err_cb(*len, arg);
 			return 0;
 		}
 		if (hdr->nlmsg_type == NLMSG_ERROR) {
@@ -49,7 +47,8 @@ static int nlmsg_receive(char *buf, int len,
 
 static int rtnl_return_err(int err, void *arg)
 {
-	pr_warn("ERROR %d reported by netlink\n", err);
+	errno = -err;
+	pr_perror("ERROR %d reported by netlink", err);
 	return err;
 }
 
