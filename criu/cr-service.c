criupatchwork/criu

commit 6e04f3b2dd0602dc0024e6d326d74e6124d279fc

criu: call kerndat_init() after log_init()
We don't have rights to print anything into standard descriptors
and we want to have all output in a specified log file.

Reviewed-by: Kirill Tkhai <ktkhai@virtuozzo.com>
Signed-off-by: Andrei Vagin <avagin@virtuozzo.com>

criu/cr-service.c
@@ -371,6 +371,9 @@ static int setup_opts_from_req(int sk, CriuOpts *req)
 		goto err;
 	}
 
+	if (kerndat_init())
+		return 1;
+
 	if (log_keep_err()) {
 		pr_perror("Can't tune log");
 		goto err;

criu/crtools.c
@@ -58,9 +58,6 @@ static int early_init(void)
 	if (init_service_fd())
 		return 1;
 
-	if (kerndat_init())
-		return 1;
-
 	return 0;
 }
 
@@ -212,6 +209,9 @@ int main(int argc, char *argv[], char *envp[])
 	}
 
 	if (log_init(opts.output))
+		return 1;
+
+	if (kerndat_init())
 		return 1;
 
 	if (opts.deprecated_ok)

commit 77c843ef63cee8362e37d6677e56700a01c58c92

service: call kerndat_init() before feature check
setup_opts_from_req() sets up logging and call kerndat_init().

Investigated-by: Adrian Reber <areber@redhat.com>
Fixes: 6e04f3b2dd06 ("criu: call kerndat_init() after log_init()")
Signed-off-by: Andrei Vagin <avagin@gmail.com>

criu/cr-service.c
@@ -971,6 +971,9 @@ static int handle_feature_check(int sk, CriuReq * msg)
 	}
 
 	if (pid == 0) {
+		/* kerndat_init() is called from setup_opts_from_req() */
+		if (setup_opts_from_req(sk, msg->opts))
+			exit(1);
 
 		setproctitle("feature-check --rpc");
 
@@ -994,7 +997,7 @@ static int handle_feature_check(int sk, CriuReq * msg)
 		 * be send from the parent process.
 		 */
 		ret = send_criu_msg(sk, &resp);
-		exit(ret);
+		exit(!!ret);
 	}
 	if (waitpid(pid, &status, 0) != pid) {
 		pr_perror("Unable to wait %d", pid);