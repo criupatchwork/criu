9aae8443fd6e1d114ead90008ddc06633829a823
Cyrill Gorcunov <gorcunov@gmail.com>
2018-12-07 14:30:42 +0300

    epoll: Add ability to restore migrated targets
    Some of targets file might belong to a different process:
    say main epoll descriptor is inherited on fork but child
    opens new file and add it into parent's epoll. To handle
    such situation we use saved pid of a process which owns
    target and on restore stage just queue this target in
    fdstore engine. Then we notify the waiters which fetch
    the target and add it into epoll descriptor.
    
    Signed-off-by: Cyrill Gorcunov <gorcunov@gmail.com>

--- criu/cr-restore.c
+++ criu/cr-restore.c
@@ -251,6 +251,9 @@ static int crtools_prepare_shared(void)
 	if (prepare_cgroup())
 		return -1;
 
+	if (eventpoll_prepare_shared())
+		return -1;
+
 	return 0;
 }
 
@@ -378,6 +381,10 @@ static int root_prepare_shared(void)
 		goto err;
 
 	ret = add_fake_unix_queuers();
+	if (ret)
+		goto err;
+
+	ret = eventpoll_prepare_targets();
 	if (ret)
 		goto err;
 
